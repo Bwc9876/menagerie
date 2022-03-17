d9e4ac5f929d577ad92d347a1e7e49a4f7d5c197
Ben C <bwc9876@gmail.com>
2022-03-17 05:16:34 -0700

    Static byte mode

--- menagerie/Items/Static/StaticItem.py
+++ menagerie/Items/Static/StaticItem.py
@@ -4,6 +4,7 @@ from menagerie.Items.AbstractItem import AbstractItem
 class StaticItem(AbstractItem):
 
     extensions = ('*',)
+    byte_mode = True
     
     def initialize(self) -> None:
         pass
