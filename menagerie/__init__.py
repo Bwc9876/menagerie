d1bb80f63bc362844018007da1ba198fb1ed6857
Ben C <bwc9876@gmail.com>
2022-04-16 13:49:37 -0400

    Bump version

--- menagerie/__init__.py
+++ menagerie/__init__.py
@@ -1,7 +1,7 @@
 from menagerie.generate import generate
 from menagerie.new_project import new_project
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __help__ = """
                 Usage:
                     menagerie new-project [name] : Creates a new menagerie project with a given name
