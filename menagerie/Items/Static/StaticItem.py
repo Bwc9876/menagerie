943860b9ca4011954a5cafcc60855e5f7874e667
Ben C <bwc9876@gmail.com>
2022-04-21 00:33:44 -0400

    Delete

--- menagerie/Items/Static/StaticItem.py
+++
@@ -1,9 +0,0 @@
-from menagerie.Items.AbstractItem import AbstractItem
-
-
-class StaticItem(AbstractItem):
-    extensions = ('*',)
-    byte_mode = True
-
-    def initialize(self) -> None:
-        pass
--- menagerie/project_template/content/pages/index.md
+++
@@ -1,6 +0,0 @@
-Title: Home
-Sort-Priority: 100
-
-# Welcome!
-
-You've successfully generated a menagerie project!
