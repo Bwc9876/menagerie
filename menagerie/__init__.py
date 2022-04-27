7bdce6b1392604e07735f93a09e8b96a2a76c03f
Ben C <bwc9876@gmail.com>
2022-04-27 12:56:10 -0400

    Switch to use the `argparse` library (#2)
    * Start switch to argparse
    
    * Fix sys.argv
    
    * Change `log_error` to use `sys.exit`
    
    * Made compatible with runpy
    
    * Remove __help__

--- menagerie/__init__.py
+++ menagerie/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from pathlib import Path
 from textwrap import dedent
 
@@ -6,31 +7,33 @@ from menagerie.generate import generate
 from menagerie.new_project import new_project
 
 __version__ = '0.1.1'
-__help__ =  dedent("""
-                Usage:
-                    menagerie new-project [name] : Creates a new menagerie project with a given name
-                    menagerie generate [config] : Generates a site with the given config
-                    menagerie help : Shows this message
-                    menagerie version : Shows the version
-            """)
+
+
+def get_parser():
+
+    # Root Parser
+    parser = argparse.ArgumentParser(description="Run a menagerie command")
+    parser.add_argument("-v", "--version", action='version', version=__version__)
+    subparsers = parser.add_subparsers(title='command', dest='command', required=True)
+
+    # New Project Parser
+    new_proj_parser = subparsers.add_parser('new-project', help="Start a new menagerie site")
+    new_proj_parser.add_argument('name', help="The new folder to create and place the new project in", default="NewProject")
+
+    # Generate Parser
+    gen_parser = subparsers.add_parser('generate', help="Generate an existing menagerie site")
+    gen_parser.add_argument("-c", "--config", help="Specify a different config file to use", default="config.json", type=Path)
+
+    return parser
 
 
 def execute_from_commandline(args: list[str] = None):
+    parser = get_parser()
     if args is None:
-        args = sys.argv
-    match [arg.lower() for arg in args[1:]]:
-        case ['new-project']:
-            new_project("NewProject")
-        case ['new-project', name]:
-            new_project(name)
-        case ['generate']:
-            generate(Path('config.json'))
-        case ['generate', Path(config_path)]:
-            generate(config_path)
-        case ['version']:
-            print(f"Menagerie Version: {__version__}")
-        case ['help']:
-            print(__help__)
-        case _:
-            print(f"Invalid Command '{' '.join(sys.argv)}'")
-            print(__help__)
+        parsed_args = parser.parse_args()
+    else:
+        parsed_args = parser.parse_args(args)
+    if parsed_args.command == 'generate':
+        generate(Path(parsed_args.config))
+    elif parsed_args.command == 'new-project':
+        new_project(parsed_args.name)
