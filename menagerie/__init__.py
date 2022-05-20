2084c18fbec0681ec5099b5d00558acfce2c054e
Ben C <bwc9876@gmail.com>
2022-05-20 19:30:49 -0400

    Bump version

--- menagerie/__init__.py
+++ menagerie/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from menagerie.generate import generate
 from menagerie.new_project import new_project
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 
 def get_parser():
