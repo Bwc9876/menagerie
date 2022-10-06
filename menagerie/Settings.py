fec9cb72b93cc94007c078e99f7fb46f9084ccde
Ben C <bwc9876@gmail.com>
2022-10-06 11:47:48 -0400

    Made URL_PREFIX=/ by default

--- menagerie/Settings.py
+++ menagerie/Settings.py
@@ -99,7 +99,7 @@ def setup_settings(config_path: Path):
         new_config.update(NestedDict(config))
         new_config['out_dir'] = Path(new_config['paths', 'out'])
         new_config['content_dir'] = Path(new_config['paths', 'content'])
-        new_config['url_prefix'] = getenv("URL_PREFIX", "")
+        new_config['url_prefix'] = getenv("URL_PREFIX", "/")
         new_config['config_path'] = config_path
         new_config['themes', 'theme-opposite'] = "light" if new_config['themes', 'theme'] == 'dark' else "dark"
         Logger.update_level_from_string(new_config['log_level'])
